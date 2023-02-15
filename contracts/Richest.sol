69d3ea319a02dfdfded0d3b6880413c7fd234b44
mainak4ever <mainak4ever@gmail.com>
2023-02-16 02:52:52 +0530

    richest commit

--- contracts/Richest.sol
+++ contracts/Richest.sol
@@ -2,6 +2,33 @@ pragma solidity >=0.4.22 <=0.8.17;
 
 contract Richest {
     // Write your code here
+    mapping (address => uint256) pendingWithdrawls;
+    address richest;
+    uint mostsent;
+
+    function becomeRichest() external payable returns(bool) {
+        if(msg.value > mostsent){
+            pendingWithdrawls[richest] += mostsent;
+            richest = msg.sender;
+            mostsent = msg.value;
+            return true;
+        }
+        return false;
+    }
+
+    function withdraw() external {
+        uint value = pendingWithdrawls[msg.sender];
+        pendingWithdrawls[msg.sender]=0;
+        payable(msg.sender).transfer(value);
+    }
+
+    function getRichest() public view returns(address){
+        return richest;
+    }
+}
+
+/*contract Richest {
+    // Write your code here
     mapping (address => uint256) users;
     //mapping (address => uint256) pendingWithdrawls;
     address richest;
@@ -28,3 +55,4 @@ contract Richest {
         return richest;
     }
 }
+*/
